cebf0f5e31cdf47a7e29ae01aee4abc2c89a0ee7
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-25 08:19:12 +0900

    ルーティング設定、コントローラー作成

---
+++ app/controllers/main_profiles_controller.rb
@@ -0,0 +1,2 @@
+class MainProfilesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :dreams
+  resources :dreams do
+    resources :main_profiles
+  end
   root to:"dreams#index"
 end

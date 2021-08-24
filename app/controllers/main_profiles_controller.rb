a13ae2cb1b587f320d027ce45a90d114009e085b
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-25 08:25:11 +0900

    コントローラー設定

--- app/controllers/main_profiles_controller.rb
+++ app/controllers/main_profiles_controller.rb
@@ -1,2 +1,12 @@
-class MainProfilesController < ApplicationController
+class MainProfilesController < ApplicationControlle
+  def new
+    @main_profile = MainProfile.new
+  end
+  def create
+    @main_profile = MainProfile.new
+    if @main_profile.create
+      redirect_to "dreams#show"
+    else
+      render :new
+    end
 end

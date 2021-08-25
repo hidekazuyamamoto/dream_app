38ebac969269f82e6bb0d0ae2bb8f30fce4f8481
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-25 11:16:31 +0900

    itemsコントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+  def new
+    @item=Item.new
+  end
+  def create
+    @item=Item.new(params_item)
+    if @item.save
+      redirect_to 
+    else
+      render :new
+    end
+  end
 end

6c8a4354be3afe1e6724e0a415317c057de0a5c6
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-25 11:04:03 +0900

    編集機能実装

--- app/controllers/main_profiles_controller.rb
+++ app/controllers/main_profiles_controller.rb
@@ -10,6 +10,17 @@ class MainProfilesController < ApplicationController
       render :new
     end
   end
+  def edit
+    @main_profile = MainProfile.where(user_id: params[:id]).first
+  end
+  def update
+    @main_profile = MainProfile.where(user_id: params[:id]).first
+    if @main_profile.update(params_main_profile)
+      redirect_to dream_path(current_user.id)
+    else
+      render :edit
+    end
+  end
   private
   def params_main_profile
     params.require(:main_profile).permit(:name, :user_profile, :image, :mission, :mission_ex, :URL_twitter, :URL_facebook, :URL_instagram).merge(user_id: current_user.id)

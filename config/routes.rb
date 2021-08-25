e4bd8075814cf483e8fae0efe8ac6225207bc7e4
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-25 09:02:03 +0900

    ビュー作成、保存完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :dreams do
-    resources :main_profiles
-  end
+  resources :dreams
+  resources :main_profiles
   root to:"dreams#index"
 end

e31178bb2944ecb1803b0600bacb1ffc17d8733d
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-25 10:00:23 +0900

    マイページプロフィール欄実装完了

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -2,6 +2,7 @@ class DreamsController < ApplicationController
   def index
   end
   def show
+    @user = User.find(current_user.id)
     @main_profile = MainProfile.where(user_id: current_user.id).first
   end
 end

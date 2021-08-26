2cb2ca18a292efdcc33fe9f3a940b49e234ce8c0
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-27 07:42:41 +0900

    js追加機能

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,6 +1,12 @@
 function display(){
   const menuicon = document.getElementById("menuicon")
   const menuber =document.getElementById("menuber")
+  menuicon.addEventListener("mouseover", ()=>{
+    menuicon.setAttribute("style", "border: 3px solid black;")
+  })
+  menuicon.addEventListener("mouseout",()=>{
+    menuicon.removeAttribute("style", "border: 3px solid black;")
+  })
   menuicon.addEventListener("click",()=>{
     if(menuber.getAttribute("style") == "display: block"){
         menuber.removeAttribute("style" ,"display: block;")

ab4f1b6448b92ef5c6bdbc7d11b5d08f69e8a3c4
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-26 10:31:15 +0900

    マイグレーション修正

--- db/migrate/20210826010922_create_sub_purchased_logs.rb
+++ db/migrate/20210826010922_create_sub_purchased_logs.rb
@@ -4,10 +4,10 @@ class CreateSubPurchasedLogs < ActiveRecord::Migration[6.0]
 
       t.timestamps
       t.references :item
-      t.string :quantitiy
-      t.string :price
+      t.string :quantitiy, nill:false
+      t.string :price, nill: false
       t.string :name, null: false
-      t.string :postal_code
+      t.string :postal_code, nill: false
       t.integer :area_id, nill: false
       t.string :city, nill: false
       t.string :address, nill:false

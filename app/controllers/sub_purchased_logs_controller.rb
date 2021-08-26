ca11b2ce22ebf9d48796e2c2413dd61ed6152edc
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-26 10:38:05 +0900

    ルーティング、コントローラー作成

---
+++ app/controllers/sub_purchased_logs_controller.rb
@@ -0,0 +1,2 @@
+class SubPurchasedLogsController < ApplicationController
+end
---
+++ test/controllers/sub_purchased_logs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SubPurchasedLogsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

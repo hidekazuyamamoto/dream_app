16ca52d636984e96c4367bb254d8ed4268d704b0
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-26 10:43:54 +0900

    モデル微修正

--- app/models/where.rb
+++ app/models/where.rb
@@ -2,7 +2,7 @@ class Where < ActiveHash::Base
   self.data = [
     { id: 1, name: '特になし' },
     { id: 2, name: 'Twitterで知った' },
-    { id: 3, name: 'Facebookで知った' }
+    { id: 3, name: 'Facebookで知った' },
     { id: 4, name: 'Instagramで知った' }
   ]
 

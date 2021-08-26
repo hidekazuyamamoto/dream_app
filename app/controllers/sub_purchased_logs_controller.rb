c65f8de269083c321036ece163ca61889b14ce9c
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-26 10:44:09 +0900

    購入ページ作成

--- app/controllers/sub_purchased_logs_controller.rb
+++ app/controllers/sub_purchased_logs_controller.rb
@@ -1,2 +1,8 @@
 class SubPurchasedLogsController < ApplicationController
+  def new
+    @item = Item.find(params[:item_id])
+    @sub_purchased_log = SubPurchasedLog.new
+  end
+  def create
+  end 
 end

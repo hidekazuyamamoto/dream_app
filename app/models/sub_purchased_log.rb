24291db0b8a2e74cea8e7f964f7616261b6bede5
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-26 10:34:23 +0900

    バリテーション、アソシエーション、アクティブハッシュ作成

--- app/models/sub_purchased_log.rb
+++ app/models/sub_purchased_log.rb
@@ -1,2 +1,16 @@
 class SubPurchasedLog < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :where
+  belongs_to :area
+
+  belongs_to :item
+
+  with_options presence: true do
+    validates :quantitiy
+    validates :price
+    validates :name
+    validates :postal_code
+    validates :city
+    validates :address
+  end
 end

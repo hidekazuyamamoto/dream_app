7dd258f36f3be01b3011fba71e4f44846e15c939
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-31 09:39:13 +0900

    画面が暗くなる

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,6 +1,8 @@
 function display(){
   const menuicon = document.getElementById("menuicon")
   const menuber =document.getElementById("menuber")
+  const fadeLayer = document.getElementById("fadeLayer")
+  const body_height = document.body.scrollHeight 
   menuicon.addEventListener("mouseover", ()=>{
     menuicon.setAttribute("style", "border: 3px solid black;")
   })
@@ -11,6 +13,8 @@ function display(){
     if(menuber.getAttribute("style") == "display: block"){
         menuber.removeAttribute("style" ,"display: block;")
     }else{
+        fadeLayer.setAttribute("style", "display: block")
+        fadeLayer.style.height = document.body.scrollHeight+"px"
         menuber.setAttribute("style", "display: block")
     }
   })
